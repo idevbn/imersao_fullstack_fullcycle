fbd9d2fc80de4c0565c3ccad5a2c664fb6733c70
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-05 13:50:54 -0300

    feat: Criado projeto do nextjs
    O projeto é referente ao frontend da aplicação e foi construído nas aulas 3 e 4
    
    OBS: o arquivo .env naturalmente é restrito à visualização e é adicionado ao .gitignore,
    mas foi seguido o modelo do projeto.

---
+++ nextjs/src/app/pagina1/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+function Pagina1() {
+    return (
+        <div>
+            <h1>Dá-lhe AUTO!</h1>
+            <Button variant="contained" color="primary">AUTO</Button>
+        </div>
+    );
+}
+
+export default Pagina1;
